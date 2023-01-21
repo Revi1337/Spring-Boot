1e6013fb742b8e707c9b7b4bbe0264611f150fb5
Revi1337 <david122123@gmail.com>
2023-01-22 00:57:53 +0900

    Security SecurityContextPersistenceFilter
    Security SecurityContextHolder & SecurityContextHolder

---
+++ SpringSecuritySecurityContextPersistanceFilter/src/main/java/com/example/springsecuritysecuritycontextpersistancefilter/SpringSecuritySecurityContextPersistanceFilterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritysecuritycontextpersistancefilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySecurityContextPersistanceFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySecurityContextPersistanceFilterApplication.class, args);
+    }
+
+}
---
+++ SpringSecuritySecurityContextPersistanceFilter/src/test/java/com/example/springsecuritysecuritycontextpersistancefilter/SpringSecuritySecurityContextPersistanceFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritysecuritycontextpersistancefilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySecurityContextPersistanceFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
