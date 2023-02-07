5b34e07e6f98350220f33abdd87e52b1b5da68a0
Revi1337 <david122123@gmail.com>
2023-02-08 05:34:40 +0900

    Customizing LogoutHandler Methodology
    * Spring 의 LogoutHandler 를 Customizing 하는 3가지 방법을 구현해보았음.
    1. LogoutHandler 인터페이스를 람다식으로 구현 
    2. LogoutHandler 인터페이스를 구현해서 Bean 으로 생성
    3. LogoutHandler 인터페이스의 기본 구현체를 상속해서 구현하여 Bean 으로 생성

--- SpringSecurityLogoutFilter/src/main/java/com/example/springsecuritylogoutfilter/config/SecurityConfig.java
+++ SpringSecurityLogoutFilter/src/main/java/com/example/springsecuritylogoutfilter/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import javax.servlet.http.HttpSession;
 
@@ -22,7 +23,7 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/logout")                                       // 로그아웃 페이지 설정
                         .logoutSuccessUrl("/login")                                 // 로그아웃 성공시 이동경로. (logoutSuccessHandler 랑 같이있으면 무시됨. --> 우선순위가 밀림.)
-                        .addLogoutHandler((req, res, auth) -> {                     // 로그아웃시 실행할 Handler 명시
+                        .addLogoutHandler((req, res, auth) -> {                     // TODO LogoutHandler 를 커스터마이징 하는 방법 1.  (LogoutHandler 의 람다식 으로 구현)
                             HttpSession httpSession = req.getSession();
                             httpSession.invalidate();
                         })
@@ -33,4 +34,14 @@ public class SecurityConfig {
                 .build();
     }
 
+
+    @Bean
+    public LogoutHandler customLogoutHandler1() { // TODO LogoutHandler 를 커스터마이징 하는 방법 2. (LogoutHandler 를 구현 - 새롭게 만드는 의도)
+        return new CustomLogoutHandler1();
+    }
+
+    @Bean
+    public LogoutHandler customLogoutHandler2() { // TODO LogoutHandler 를 커스터마이징 하는 방법 3. (LogoutHandler 의 기본 구현체를 상속 - 할일을 하고 + 부모의 로직을 따라가려는 의도)
+        return new CustomLogoutHandler2();
+    }
 }
